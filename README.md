67b4f8512a5ca8f27d92d3ee4ece2f588de1418d
Prabal Pathak <prabal01pathak@gmail.com>
2021-05-29 15:15:08 +0530

    'Additional functionalty added'

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 ### Requirements 
     Python ==>3.8
     and some knowladge of python built in modules.
-> #### Modules  
+#### Modules  
 > **os** ,
 > **zipfile** ,
 > **tkinter**  ,
@@ -21,31 +21,31 @@
 ****
 
 
-> ### Files
+### Files
 >|**Gui**        | **Cli**       | 
 >|-----------|-----------|
 >|graphZip.py| zipfile.py|
 
 ****
-> ### **Excutable Files**
+### **Excutable Files**
 >   |**Gui**         | **Cli**              | 
 >   |------------|-----------------|
 >  |graphZip.exe| zipfile1.exe    | 
 
-## Contributing
+# Contributing
     Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
     Please make sure to update tests as appropriate.
 
 
-## License
+# License
 
 **[MIT](https://choosealicense.com/licenses/mit/)**
 
-### Project Status 
+# Project Status 
      Project is in devolopment stage. 
      If you have any idea then you can share.
 
-     
-### this is practice project 
+
+                                                   
 
--- zipopen.py
+++ zipopen.py
@@ -1,6 +1,11 @@
+from functools import partial
+from zipfile import ZipFile
 from tkinter import filedialog
 from tkinter import *
 import tkinter as tk
+import time
+import os
+
 
 class Application(tk.Frame):
     def __init__(self,master=None):
@@ -10,25 +15,50 @@ class Application(tk.Frame):
         self.creat_widgets()
 
     def creat_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there['text']='SELECT'
-        self.hi_there['command']= self.open_zip
-        self.hi_there.pack(side='top')
+        refresh = Button(root,text = 'Refresh',command = self.refresh)
+        refresh.pack()
+        self.ask_file = tk.Button(self,text = 'SELECT',fg = 'blue',command = self.ask_zip)
+        self.ask_file.pack()
         self.quit = tk.Button(self,text = 'QUIT',fg = 'red',command = self.master.destroy)
-        self.quit.pack(side='bottom')
-    def open_zip(self):
-        self.fill = tk.Label(root,text = f'File Name --> {self.files}\n',fg = 'green',bg = 'white')
-        self.fill.pack(side = 'left')
-        fileName.set(self.files)
-        self.fill = tk.Label(root,textvariable = fileName)
-        print(self.fill)
-                count = 0
-                print(l)
-                for i in range(len(l:
-                    print(i)
-                    self.label = Label(root,text = f'Files are {l[count].filename}\n',fg = 'blue',bg = 'white')
-                self.label.pack(side = 'left')
-                count +=1
+        self.quit.pack()
+    def ask_zip(self):
+        files = filedialog.askopenfilename(filetypes =[('zip {.zip}')])
+        data = StringVar()
+        data.set(files)
+        entry = Entry(root,textvariable = data)
+        entry.place(x=10,y=30)
+        show = Button(root,text = 'SHOW',command = partial(self.open_zip,data.get()))
+        show.place(x = 150,y=25)
+        for_extract = Button(text = 'EXTRACT ALL',command = partial(self.extract_all,files))
+        for_extract.place(x =200,y = 25)
+        self.refresh()
+    def open_zip(self,files):
+        with ZipFile(files,'r') as zips:
+            l = zips.infolist()
+            for i in range(len(l)):
+                count = Label(text = f'{i+1}.')
+                count.place(x = 1,y=i*30+60)
+                info_data = StringVar()
+                info_data.set(l[i].filename)
+                label = Entry(root,textvariable =info_data,fg = 'blue',bg = 'white')
+                label.place(x=15,y=i*30+60)
+
+    def extract_all(self,files):
+        with ZipFile(files,'r') as zips:
+            ask_folder = filedialog.askdirectory()
+            extract_path = str(ask_folder)
+            zips.extractall(path=extract_path)
+            time.sleep(1)
+            label_done = Label(root , text = 'Done' )
+            label_done.place(x = 150 , y = 90)
+            os.startfile(ask_folder)
+
+    def refresh(self):
+        self.destroy()
+        self.__init__()
+if __name__ == "__main__":
+    root = Tk()
+    root.geometry('200x200')
     app = Application(master = root)
-    app.mainloop()  
+    root.mainloop()  
 
