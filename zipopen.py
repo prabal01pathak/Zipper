0e348286ea2ee1124b675a083d03874d0f789322
Prabal Pathak <prabal01pathak@gmail.com>
2021-06-06 16:03:02 +0530

    add refresh method.

--- zipopen.py
+++ zipopen.py
@@ -2,6 +2,7 @@ from functools import partial
 from zipfile import ZipFile
 from tkinter import filedialog
 from tkinter import *
+from tkinter import ttk
 import tkinter as tk
 import time
 import os
@@ -10,68 +11,109 @@ from datetime import datetime
 
 #This is main class for handling frame and widgets.
 
-class Application(tk.Frame):
-    def __init__(self,master=None):
-        super().__init__(master)
-        self.master= master 
+class Application(ttk.Frame):
+    def __init__(self):
+        super().__init__()
         self.pack()
         self.creat_widgets()
+        
 
 #widgets for selecting file and processing .
     def creat_widgets(self):
         self.ask_file = tk.Button(self,text = 'SELECT',fg = 'blue',command = self.ask_zip) # Ask for zip file.
-        self.ask_file.pack()
+        self.ask_file.pack(side = LEFT , expand =1,padx = 5 ,pady = 4)
         self.quit = tk.Button(self,text = 'QUIT',fg = 'red',command = self.master.destroy) #exit from application.
-        self.quit.pack()
-        self.show_time = Label(self,fg = 'red')
+        self.quit.pack(side = LEFT , expand = 1,padx = 5 ,pady = 4)
+       # self.refresh = Button(self,text = 'Refresh' , command = refresh)
+       # self.refresh.pack()
+        self.show_time = Label(self,fg = 'white',bg = 'black')
         self.digital_clock()
-        self.show_time.pack()
+        self.show_time.pack(fill = X,expand = 1)
 
     #For selecting zip file.
     def digital_clock(self):
-        times = time.strftime('%H:%M:%S')
+        times = time.strftime('%H:%M')
         date = datetime.now().date()
         self.show_time.config(text =f'Date : {date} \n Time: {times}')
         self.show_time.after(200,self.digital_clock)
 
     def ask_zip(self):
+        global lists
+        lists = []
         files = filedialog.askopenfilename(filetypes =[('zip {.zip}')]) #dialog only zip files.
-        data = StringVar()
-        data.set(files) #fill data with name of file.
-        entry = Entry(root,textvariable = data) #Name of file.
-        entry.place(x=10,y=30)
-        show = Button(root,text = 'SHOW',command = partial(self.open_zip,data.get())) #show list files in archive.
-        show.place(x = 150,y=25)
-        for_extract = Button(text = 'EXTRACT ALL',command = partial(self.extract_all,files)) #Extract all member of archive.
-        for_extract.place(x =200,y = 25)
+        if len(files) == 0:
+            label = Label(root , text = "please select zipfile")
+            label.pack()
+            lists.append(label)
+            button = Button(root ,text = 'Delete',command = self.refresh)
+            button.pack()
+            lists.append(button)
+        else:
+            data = StringVar()
+            data.set(files) #fill data with name of file.
+            entry = Entry(root,textvariable = data) #Name of file.
+            entry.place(x=10,y=30)
+            lists.append(entry)
+            show = Button(root,text = 'SHOW',command = partial(self.open_zip,data.get())) #show list files in archive.
+            show.place(x = 150,y=25)
+            lists.append(show)
+            refresh = Button(root ,text = 'RFRESH', command = self.refresh)
+            refresh.pack()
+            lists.append(refresh)
+            for_extract = Button(text = 'EXTRACT ALL',command = partial(self.extract_all,files)) #Extract all member of archive.
+            for_extract.place(x =200,y = 25)
+            lists.append(for_extract)
 
     #This method will open zip file and give information about file.
 
     def open_zip(self,files):
+        vscrollbar = Scrollbar(self,orient=VERTICAL)
+        vscrollbar.pack(fill = Y,side =RIGHT ,expand = False)
+        lists.append(vscrollbar)
         with ZipFile(files,'r') as zips:
             l = zips.infolist()
             for i in range(len(l)):
                 count = Label(text = f'{i+1}.') 
+                lists.append(count)
                 count.place(x = 1,y=i*30+60)
                 info_data = StringVar()
                 info_data.set(l[i].filename)
-                label = Entry(root,textvariable =info_data,fg = 'blue',bg = 'white') #To show Name of files in archive.
-                label.place(x=15,y=i*30+60)
+                label_entry = Entry(root,textvariable =info_data,fg = 'blue',bg = 'white') #To show Name of files in archive.
+                lists.append(label_entry)
+                label_entry.place(x=15,y=i*30+60)
+                
 
     #For extracting all files from archive.
     def extract_all(self,files):
-        with ZipFile(files,'r') as zips:
-            ask_folder = filedialog.askdirectory()
-            extract_path = str(ask_folder)
-            zips.extractall(path=extract_path)
-            time.sleep(1)
-            label_done = Label(root , text = 'Done' )
-            label_done.place(x = 150 , y = 90)
-            os.startfile(ask_folder) # prompt with extracted files.
+        try:
+            with ZipFile(files,'r') as zips:
+                ask_folder = filedialog.askdirectory()
+                label = Label(root , text = ask_folder)
+                label.pack(fill = BOTH ,expand =1 ,padx = 5 ,pady = 5)
+                lists.append(label)
+                extract_path = str(ask_folder)
+                zips.extractall(path=extract_path)
+                time.sleep(1)
+                label_done = Label(root , text = 'Done' )
+                label_done.place(x = 150 , y = 90)
+                lists.append(label_done)
+                os.startfile(ask_folder) # prompt with extracted files.
+        except:
+            label_error = Label(root,text = 'error')
+            label_error.pack()
+            lists.append(label_error)
 
+    def refresh(self):
+            l =  lists
+            for i in l:
+                i.destroy()
+     
+#    self.master.destroy()
+#    root = Tk()
+#    Application(master = root)
+#    root.mainloop()
 if __name__ == "__main__":
     root = Tk()
-    root.geometry('200x200')
-    app = Application(master = root)
+    app = Application()
     root.mainloop()  
 
